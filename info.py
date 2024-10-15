303be9a3e96064f93e85f4c904e0c8211022436a
Brian Chen <brianyichen@gmail.com>
2024-10-15 09:05:24 -0500

    small stuff

--- info.py
+++ info.py
@@ -5,31 +5,31 @@ with open ('college-data.csv', 'r', encoding='utf-8') as file:
     csv_file = list(csv.reader(file))
 
 university = ""
-search = input("Enter the name of the Texas university you're interested in: ")
 search_results = []
-for line in csv_file:
-    university_names = line[1]
+while len(search_results) == 0:
+    search = input("Enter the name of the Texas university you're interested in (capitalize first letter): ")
+    for line in csv_file:
+        university_names = line[1]
 
-    university_names = university_names.split("\n")
-    for university in university_names: 
-        if search in university:
-            search_results.append(university)
+        university_names = university_names.split("\n")
+        for university in university_names: 
+            if search in university:
+                search_results.append(university)
 
 num_diff = 1
 num_same = -1
 college = ""
-if len(search_results) > 1:
-    print("Multiple results found for \"" + search + "\"")
-    print("[", 1, "]", search_results[0])
-    for i in range(len(search_results) - 1):
-        if search_results[i] != search_results[i + 1]:
-            print("[", num_diff+1, "]", search_results[i+1])
-            num_diff += 1
-        else:
-            num_same += 1
-    num_university = int(input("Select 1-" + str(num_diff) + ": "))
-    college = search_results[num_university * 9 - 2]
-    print(college)
+print("These schools match your search:")
+print("[", 1, "]", search_results[0])
+for i in range(len(search_results) - 1):
+    if search_results[i] != search_results[i + 1]:
+        print("[", num_diff+1, "]", search_results[i+1])
+        num_diff += 1
+    else:
+        num_same += 1
+num_university = int(input("Select 1-" + str(num_diff) + ": "))
+college = search_results[num_university * 9 - 2]
+print(college)
 
 ethnicities = ["Total", "White", "African American", "Hispanic", "Asian"]
 genders = ["Total", "Male", "Female", "Other"]
